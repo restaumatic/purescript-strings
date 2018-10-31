8e74acc4962d68970338068ebfb91e5c98f85c90
Maciej Bielecki <zyla@prati.pl>
2018-10-31 14:22:54 +0100

    Add more information to `charAt` crash message

--- src/Data/String/Unsafe.js
+++ src/Data/String/Unsafe.js
@@ -3,7 +3,7 @@
 exports.charAt = function (i) {
   return function (s) {
     if (i >= 0 && i < s.length) return s.charAt(i);
-    throw new Error("Data.String.Unsafe.charAt: Invalid index.");
+    throw new Error("Data.String.Unsafe.charAt: Invalid index (string: " + s + ", index: " + i + ", length: " + s.length + ")");
   };
 };
 
